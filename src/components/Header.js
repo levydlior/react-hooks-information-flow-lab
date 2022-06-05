7734e124b72bd526a08b0fc9ca751cb1d6134a0f
Lior <levydlior@gmail.com>
2022-06-05 12:08:15 -0600

    commit tree 2efd45bc3cf145cc24ab00315e8c3f3e78ed7cce

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header ( { onDarkModeClick, darkMode }){
+    return(
+    <header>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick}>
+      {darkMode ? "Dark" : "Light"} Mode
+    </button>
+  </header>)
+}
+
+export default Header
